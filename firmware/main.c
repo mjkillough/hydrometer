6a3605d4e7240c3a80fa5ea03b670ed850ef3c47
Michael Killough <michaeljkillough@gmail.com>
2020-11-22 15:10:30 +0000

    firmware: initial blinky

---
+++ firmware/main.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+#include <FreeRTOS.h>
+#include <task.h>
+
+#include <espressif/esp_common.h>
+#include <esp/uart.h>
+#include <esp8266.h>
+
+const int gpio = 2;
+
+void blink(void *pvParameters)
+{
+    gpio_enable(gpio, GPIO_OUTPUT);
+
+    while(1) {
+        gpio_write(gpio, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_write(gpio, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+void user_init(void)
+{
+    uart_set_baud(0, 115200);
+
+    xTaskCreate(blink, "blink", 256, NULL, 2, NULL);
+}
+
